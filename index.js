6de5801e89d7ce2a0e1a20aa7a1a01f3e699a9d0
Edison-Mondal <mondaledison@gmail.com>
2023-09-12 16:49:47 +0600

    init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const path = require('path');
 
 var app = express();
 
@@ -9,6 +11,51 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+// Set up the multer storage and file filter
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // specify the folder where uploaded files will be stored
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 }, // Limit file size to 5 MB
+  fileFilter: (req, file, cb) => {
+    const allowedFileTypes = /jpeg|jpg|png|gif/; // Specify allowed file types
+    const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedFileTypes.test(file.mimetype);
+    if (extname && mimetype) {
+      return cb(null, true);
+    } else {
+      cb('Error: Only image files (jpeg, jpg, png, gif) are allowed!');
+    }
+  },
+});
+
+
+
+// Handle file upload
+app.post('/upload', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded.' });
+  }
+
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    filename: originalname,
+    filetype: mimetype,
+    size: size,
+  });
 });
 
 
